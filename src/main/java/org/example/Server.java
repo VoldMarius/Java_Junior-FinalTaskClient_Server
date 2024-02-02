81de953e14b63110986fb5cb4f838ba5f59f6bba
VoldMarius <ukusil@mail.ru>
2024-02-02 16:53:36 +0300

    Finished work class Server

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -1,4 +1,43 @@
 package org.example;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+
+  // Серверный сокет
+
+    private final ServerSocket serverSocket;
+
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void runServer(){
+
+        try {
+            while (!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New user connected!");
+                ClientManager clientManager = new ClientManager(socket);
+                Thread thread = new Thread(clientManager);
+                thread.start();
+            }
+        }
+        catch (IOException e){
+            closeSocket();
+        }
+
+    }
+
+    private void closeSocket(){
+        try{
+            if (serverSocket != null) serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
